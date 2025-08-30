65ef693019cf19cf612d3da090d1aba8acecc8db
Y4shG <fpsghuley77@stu.fpsk12.net>
2025-08-30 15:19:57 -0400

    Update FoodListener.java

--- src/main/java/com/vovxb/badbreath/event/FoodListener.java
+++ src/main/java/com/vovxb/badbreath/event/FoodListener.java
@@ -5,9 +5,10 @@ import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.fabricmc.fabric.api.event.player.UseItemCallback;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.FoodItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
-import net.minecraft.item.PotionItem;
+import net.minecraft.potion.PotionUtil;
 import net.minecraft.potion.Potions;
 import net.minecraft.util.Hand;
 import net.minecraft.util.ActionResult;
@@ -17,15 +18,14 @@ public class FoodListener {
     private static int badBreathTicks = 0;
 
     public static void register() {
-        // Eating food gives permanent bad breath
         UseItemCallback.EVENT.register((PlayerEntity player, net.minecraft.world.World world, Hand hand) -> {
             ItemStack stack = player.getStackInHand(hand);
 
-            // Check if item has food component
-            if (stack.getItem().getFoodComponent() != null) {
+            // Check if item is food
+            if (stack.getItem() instanceof FoodItem) {
                 badBreathTicks = 0;
                 player.addStatusEffect(new StatusEffectInstance(
-                        BadBreathMod.BAD_BREATH.value(), // RegistryEntry<StatusEffect>
+                        BadBreathMod.BAD_BREATH,
                         Integer.MAX_VALUE,
                         0,
                         false,
@@ -35,8 +35,8 @@ public class FoodListener {
 
             // Water potion cures bad breath
             if (stack.isOf(Items.POTION)) {
-                if (((PotionItem) stack.getItem()).getPotion() == Potions.WATER) {
-                    player.removeStatusEffect(BadBreathMod.BAD_BREATH.value());
+                if (PotionUtil.getPotion(stack) == Potions.WATER) {
+                    player.removeStatusEffect(BadBreathMod.BAD_BREATH);
                     badBreathTicks = 0;
                 }
             }
@@ -47,10 +47,10 @@ public class FoodListener {
         // Each tick increases amplifier
         ServerTickEvents.END_WORLD_TICK.register(world -> {
             world.getPlayers().forEach(player -> {
-                if (player.hasStatusEffect(BadBreathMod.BAD_BREATH.value())) {
+                if (player.hasStatusEffect(BadBreathMod.BAD_BREATH)) {
                     badBreathTicks++;
                     player.addStatusEffect(new StatusEffectInstance(
-                            BadBreathMod.BAD_BREATH.value(),
+                            BadBreathMod.BAD_BREATH,
                             Integer.MAX_VALUE,
                             badBreathTicks,
                             false,
