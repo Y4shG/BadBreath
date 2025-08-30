9cb5059db6f85214924baea5792dd42aa3a694d8
Y4shG <fpsghuley77@stu.fpsk12.net>
2025-08-30 15:07:38 -0400

    Update BadBreathMod.java

--- src/main/java/com/vovxb/badbreath/BadBreathMod.java
+++ src/main/java/com/vovxb/badbreath/BadBreathMod.java
@@ -3,21 +3,27 @@ package com.vovxb.badbreath;
 import com.vovxb.badbreath.effect.BadBreathEffect;
 import com.vovxb.badbreath.event.FoodListener;
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.entity.effect.StatusEffect;
-import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.RegistryKey;
 import net.minecraft.util.Identifier;
+import net.minecraft.registry.entry.RegistryEntry;
 
 public class BadBreathMod implements ModInitializer {
     public static final String MODID = "badbreath";
-    public static final StatusEffect BAD_BREATH = new BadBreathEffect();
+
+    public static RegistryEntry<BadBreathEffect> BAD_BREATH;
 
     @Override
     public void onInitialize() {
-        // hmmm effecty boy give me bad stinky breath
-        Registry.register(Registries.STATUS_EFFECT, new Identifier(MODID, "bad_breath"), BAD_BREATH);
+        // status
+        BAD_BREATH = Registry.register(
+                Registries.STATUS_EFFECT,
+                new Identifier(MODID, "bad_breath"),
+                new BadBreathEffect()
+        );
 
-        // listner lol
+        // lizter
         FoodListener.register();
     }
 }
