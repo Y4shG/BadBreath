ab86fc7e89181bf2b0ec73f766c9c764c7db1473
Y4shG <fpsghuley77@stu.fpsk12.net>
2025-08-30 14:45:19 -0400

    add files

---
+++ src/client/java/com/vovxb/badbreath/BadbreathClient.java
@@ -0,0 +1,10 @@
+package com.vovxb.badbreath;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BadbreathClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
