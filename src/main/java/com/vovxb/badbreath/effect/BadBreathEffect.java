9dae1f9cc868b29af602d2e694d5be767ea43d7e
Y4shG <fpsghuley77@stu.fpsk12.net>
2025-08-30 15:13:42 -0400

    Update BadBreathEffect.java

--- src/main/java/com/vovxb/badbreath/effect/BadBreathEffect.java
+++ src/main/java/com/vovxb/badbreath/effect/BadBreathEffect.java
@@ -5,7 +5,6 @@ import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectCategory;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.world.World;
-import net.minecraft.world.ServerWorld;
 import net.minecraft.particle.ParticleTypes;
 import net.minecraft.util.math.Box;
 import net.minecraft.entity.damage.DamageSource;
@@ -16,7 +15,6 @@ public class BadBreathEffect extends StatusEffect {
         super(StatusEffectCategory.HARMFUL, 0x00FF00); // green color
     }
 
-    // Updated for 1.21.8 API
     @Override
     public void applyUpdateEffect(LivingEntity entity, StatusEffectInstance effect) {
         int ticksElapsed = effect.getAmplifier();
@@ -31,7 +29,7 @@ public class BadBreathEffect extends StatusEffect {
             Box box = entity.getBoundingBox().expand(radius);
             world.getOtherEntities(entity, box).forEach(e -> {
                 if (e instanceof LivingEntity living && living != entity) {
-                    // Use standard MAGIC damage
+                    // MAGIC damage
                     living.damage(DamageSource.MAGIC, damage);
                 }
             });
