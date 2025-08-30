d78dff02b7d2318983dc020b5985b5b4051d50ae
Y4shG <fpsghuley77@stu.fpsk12.net>
2025-08-30 14:59:21 -0400

    Update FoodListener.java

--- src/main/java/com/vovxb/badbreath/event/FoodListener.java
+++ src/main/java/com/vovxb/badbreath/event/FoodListener.java
@@ -6,32 +6,38 @@ import net.fabricmc.fabric.api.event.player.UseItemCallback;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
-import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.ActionResult;
 
 public class FoodListener {
     private static int badBreathTicks = 0;
 
     public static void register() {
-        // PERMANANT BAD BREATH EWWWWWW
+        // BAD BREATH EWWW
         UseItemCallback.EVENT.register((player, world, hand) -> {
             ItemStack stack = player.getStackInHand(hand);
 
             if (stack.isFood()) {
                 badBreathTicks = 0; // reset timer
-                player.addStatusEffect(new StatusEffectInstance(BadBreathMod.BAD_BREATH, Integer.MAX_VALUE, 0, false, true));
+                player.addStatusEffect(new StatusEffectInstance(
+                        BadBreathMod.BAD_BREATH,
+                        Integer.MAX_VALUE,
+                        0,
+                        false,
+                        true
+                ));
             }
 
-            // drinking tasty h2O is da cure
+            // mmm h2O cures da bad stinky breath
             if (stack.isOf(Items.POTION) &&
                 stack.getOrCreateNbt().getString("Potion").equals("minecraft:water")) {
                 player.removeStatusEffect(BadBreathMod.BAD_BREATH);
                 badBreathTicks = 0;
             }
 
-            return TypedActionResult.pass(stack);
+            return ActionResult.PASS;
         });
 
-        // every tick makes your breath worseeeee
+        // Every tick makes your breath worse
         ServerTickEvents.END_WORLD_TICK.register(world -> {
             world.getPlayers().forEach(player -> {
                 if (player.hasStatusEffect(BadBreathMod.BAD_BREATH)) {
@@ -41,7 +47,8 @@ public class FoodListener {
                             Integer.MAX_VALUE,
                             badBreathTicks, // amplifier stores time
                             false,
-                            true));
+                            true
+                    ));
                 }
             });
         });
