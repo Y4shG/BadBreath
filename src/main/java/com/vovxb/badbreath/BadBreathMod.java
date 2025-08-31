6a71a2e71719ff4a8c89ef8c8461b316d752213c
Y4shG <fpsghuley77@stu.fpsk12.net>
2025-08-31 18:33:09 -0400

    Update BadBreathMod.java

--- src/main/java/com/vovxb/badbreath/BadBreathMod.java
+++ src/main/java/com/vovxb/badbreath/BadBreathMod.java
@@ -1,27 +1,59 @@
+
 package com.vovxb.badbreath;
 
 import com.vovxb.badbreath.effect.BadBreathEffect;
-import com.vovxb.badbreath.event.FoodListener;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.player.UseItemCallback;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.util.ActionResult;
 import net.minecraft.util.Identifier;
 
 public class BadBreathMod implements ModInitializer {
     public static final String MODID = "badbreath";
-    public static net.minecraft.registry.entry.RegistryEntry<StatusEffect> BAD_BREATH;
+    public static RegistryEntry<StatusEffect> BAD_BREATH;
 
     @Override
     public void onInitialize() {
-        // hmmm effecty boy give me bad stinky breath
         BAD_BREATH = Registry.register(
             Registries.STATUS_EFFECT,
-            net.minecraft.util.Identifier.of(MODID, "bad_breath"),
+            Identifier.of(MODID, "bad_breath"),
             new BadBreathEffect()
         );
 
-        // listner lol
-        FoodListener.register();
+        // Give bad breath after eating anything
+        UseItemCallback.EVENT.register((player, world, hand) -> {
+            ItemStack stack = player.getStackInHand(hand);
+            if (stack.isFood()) {
+                player.addStatusEffect(new StatusEffectInstance(
+                    BAD_BREATH,
+                    20 * 60, // 1 minute duration
+                    0,
+                    false,
+                    true
+                ));
+            }
+            // Remove bad breath if drinking water bottle
+            if (stack.isOf(Items.POTION) && stack.getOrCreateNbt().getString("Potion").equals("minecraft:water")) {
+                player.removeStatusEffect(BAD_BREATH);
+            }
+            return ActionResult.PASS;
+        });
+
+        // Every tick: apply bad breath effects
+        ServerTickEvents.END_WORLD_TICK.register(world -> {
+            for (PlayerEntity player : world.getPlayers()) {
+                if (player.hasStatusEffect(BAD_BREATH)) {
+                    BadBreathEffect.spreadBadBreath(player);
+                }
+            }
+        });
     }
 }
