9ae5dffbe2c26e4c45e4358459ba50b7d516d432
Y4shG <fpsghuley77@stu.fpsk12.net>
2025-08-31 18:37:40 -0400

    Update BadBreathEffect.java

--- src/main/java/com/vovxb/badbreath/effect/BadBreathEffect.java
+++ src/main/java/com/vovxb/badbreath/effect/BadBreathEffect.java
@@ -1,12 +1,11 @@
-
 package com.vovxb.badbreath.effect;
 
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectCategory;
-import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.particle.ParticleTypes;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.world.World;
 
 public class BadBreathEffect extends StatusEffect {
@@ -15,24 +14,27 @@ public class BadBreathEffect extends StatusEffect {
     }
 
     // Called from tick event in BadBreathMod
-    public static void spreadBadBreath(PlayerEntity player) {
-        World world = player.getWorld();
+    public static void spreadBadBreath(PlayerEntity player, World world) {
         // Spawn green particles from mouth
         if (world.isClient) {
-            world.addParticle(ParticleTypes.ITEM_SLIME,
+            world.spawnParticles(
+                ParticleTypes.ITEM_SLIME,
                 player.getX(),
                 player.getEyeY(),
                 player.getZ(),
+                1,
                 (world.random.nextDouble() - 0.5) * 0.2,
                 0.05,
-                (world.random.nextDouble() - 0.5) * 0.2);
+                (world.random.nextDouble() - 0.5) * 0.2,
+                0.0
+            );
         }
         // Harm nearby entities
-        if (!world.isClient) {
+        if (!world.isClient && world instanceof ServerWorld serverWorld) {
             double radius = 2.0D;
             world.getOtherEntities(player, player.getBoundingBox().expand(radius)).forEach(e -> {
                 if (e instanceof LivingEntity living && living != player) {
-                    living.damage(living.getDamageSources().magic(), 1.0F);
+                    living.damage(serverWorld, living.getDamageSources().magic(), 1.0F);
                 }
             });
         }
